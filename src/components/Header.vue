2682ada5f23ffec293410c9dbc3e7eefe67abb31
Jordy Tshibangu <jordytshibss@gmail.com>
2021-04-14 23:56:56 +0200

    fix : Header component info

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,6 @@
     <b-nav tabs>
       <b-nav-item disabled>Quiz App - Test Your Brain </b-nav-item>
       <b-nav-item disabled>Total Score : {{ numberCorrect}}/{{ numberTotal }}</b-nav-item>
-      This app is a used to test your general knowledge
     </b-nav>
   </div>
 </template>
